449079df17e6eaeb3e36d77986867e074adc0158
Katelyn Manuel <katelynmanuel@Katelyns-MacBook-Pro.local>
2018-10-02 19:06:51 -0600

    Fixing mongodb connection for deployment.

--- server.js
+++ server.js
@@ -31,11 +31,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mongoscraperdb", { useNewUrlParser: true });
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraperdb";
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Start the server
 app.listen(PORT, function() {
